8707fea5727060cee0167b92bb827760e8608f5b
DELEAGE <frederik.deleage@cri-paris.org>
2014-09-23 13:02:15 +0200

    solutions

---
+++ exercices/007/solution.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 23 09:39:35 2014
+
+@author: DeleageF
+"""
+words ="H","e","l","l","o"," ","W","o","r","l","d"," ","!"
+for w in words:
+     print(w)
