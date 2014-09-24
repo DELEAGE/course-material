51b0042d69d1f283cc78b6f4b1f5495024a4d430
DELEAGE <frederik.deleage@cri-paris.org>
2014-09-24 12:51:26 +0200

    2 new

---
+++ exercices/030/solution.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 12:15:05 2014
+r = list (range(1,101))
+for s in r:
+    print (s)
+
+@author: DeleageF
+"""
+
+    
+    
+ 
+
---
+++ exercices/040/solution.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 12:15:05 2014
+
+r =list(range(0, 101, 2))
+s = sum(r)
+print (s)
+
+@author: DeleageF
+"""
+
+    
+    
+ 
+
